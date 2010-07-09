e3a8fefbb0ab31ebcbeac51fc4759502c71ceecb
kensnyder <kendsnyder@gmail.com>
2010-07-09 12:47:33 -0600

    added unsetData function. made a bit more robust.

--- src/dom/element/data.js
+++ src/dom/element/data.js
@@ -1,33 +1,45 @@
   (function(plugin) {
 
-		fuse.dom.ids = {};
+		fuse.dom.dataStores = {};
 
     plugin.setData = function setData(key, value) {
 			var id, p;
-      if (arguments.length == 2) {
-				id = this.identify();
-				if (!(id in fuse.dom.ids)) {
-					fuse.dom.ids[id] = {};
-				}
-				fuse.dom.ids[id][expando + key] = value;
-			}
-			else {
+      if (arguments.length == 1) {
 				for (p in key) {
+					if (!key.hasOwnProperty(p)) {
+						continue;
+					}
 					this.setData(p, key[p]);
 				}
+			}
+			else {
+				id = this.identify();
+				if (!(id in fuse.dom.dataStores)) {
+					fuse.dom.dataStores[id] = {};
+				}
+				fuse.dom.dataStores[id][expando + key] = value;
 			}
 			return this;
     };
 
     plugin.getData = function getData(key) {
 			var id = this.identify();
-			if (!(id in fuse.dom.ids)) {
+			if (!(id in fuse.dom.dataStores)) {
 				return undefined;
 			}
-			return fuse.dom.ids[id][expando + key];
+			return fuse.dom.dataStores[id][expando + key];
+    };
+
+    plugin.unsetData = function unsetData(key) {
+			var id = this.identify();
+			if (id in fuse.dom.dataStores) {
+				delete fuse.dom.dataStores[id][expando + key];
+			}
+			return this;
     };
 
     // prevent JScript bug with named function expressions
     var setData = nil,
-      getData =   nil;
+      getData =   nil,
+			unsetData = nil;
   })(Element.plugin);
